0f6bab329638e1b401de334f11e84864af8aa3fc
Rahul Ganesh <rahulganeshrg22@gmail.com>
2025-12-07 20:16:45 -0700

    Allow origin after deployment

--- main.py
+++ main.py
@@ -27,6 +27,8 @@ origins = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://515-group1.vercel.app",
+    "https://ser515-group1-frontend-repo.onrender.com",
 ]
 
 app.add_middleware(
